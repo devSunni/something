e799bd9a51d2f76487300011871bc27e057e970d
devSunni <yangsunmo@gmail.com>
2023-04-17 22:15:54 +0900

    success 1717

--- CertiBase/CertiBase/CertiBase/CertiBase.cpp
+++ CertiBase/CertiBase/CertiBase/CertiBase.cpp
@@ -1,10 +1,49 @@
 ﻿#include <iostream>
+#include <vector>
 
-int N;
+using namespace std;
+
+vector<int> parent;
+
+int n, m;
+
+void init()
+{
+	for (int i = 0; i <= n; i++)
+	{
+		parent.push_back(i);
+	}
+}
+
+int findU(int x)
+{
+	if (parent[x] == x) return x;
+	return parent[x] = findU(parent[x]);
+}
+
+void UnionU(int x, int y)
+{
+	int xx = findU(x);
+	int yy = findU(y);
+	parent[yy] = xx;
+}
 
 int main()
 {
-    scanf("%d", &N);
+	scanf("%d %d", &n, &m);
+	init();
+	int q, a, b;
+	for (int t = 0; t < m; t++) {
+		scanf("%d %d %d", &q, &a, &b);
 
-    return 0;
+		if (q == 0) {
+			UnionU(a, b);
+		}
+		else {
+			if (findU(a) == findU(b))
+				printf("yes\n");
+			else
+				printf("no\n");
+		}
+	}
 }
