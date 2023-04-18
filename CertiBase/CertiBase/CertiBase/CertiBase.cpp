dd70b59b73f8998d3dccbc065a9af2786dff8ffa
devSunni <yangsunmo@gmail.com>
2023-04-19 00:09:20 +0900

    LCA가 아니고서야 안되는건가..

--- CertiBase/CertiBase/CertiBase/CertiBase.cpp
+++ CertiBase/CertiBase/CertiBase/CertiBase.cpp
@@ -18,13 +18,10 @@ int M;
 
 int Parent[MaxNode];
 set<int> pp[MaxNode];
-priority_queue<int, vector<int>> pq[MaxNode];
 
 void init() {
     for (int i = 0; i < MaxNode; i++) {
         Parent[i] = i;
-        priority_queue<int, vector<int>> temp;
-        pq[i] = temp;
         pp[i].clear();
     }
 }
@@ -32,8 +29,6 @@ void init() {
 int findP(int x) {
     pp[x].insert(Parent[x]);
     if (Parent[x] == x) return x;
-    //pp[x].insert(Parent[x]);
-    /*pq[x].push(Parent[x]);*/
     int p = findP(Parent[x]);
     return Parent[x] = p;
 }
@@ -43,7 +38,6 @@ void unionP(int x, int y) {
     int yy = findP(y);
     if (xx == yy) return;
     Parent[yy] = xx;
-    //pq[y].push(x);
     pp[y].insert(x);
     pp[y].insert(pp[x].begin(), pp[x].end());
     return;
@@ -78,25 +72,11 @@ int main()
         Answer.resize(MaxNode);
         set_intersection(pp[a].begin(), pp[a].end(), pp[b].begin(), pp[b].end(), Answer.begin());
 
-        int ans;
-
-        int loop = pp[a].size();
-
-        if (loop > pp[b].size())
-            loop = pp[b].size();
-
-        for (int i = 0; i < MaxNode; i++)
-        {
-            if (Answer[i] == 0)
-                break;
-
-            ans = Answer[i];
-        }
+        int ans = upper_bound(Answer.rbegin(), Answer.rend(), 0) - Answer.rbegin();
         
+        ans = Answer[MaxNode - ans - 1];
         printf("%d\n", ans);
     }
-
-
 
     return 0;
 }
