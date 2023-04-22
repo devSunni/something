df0bf4a2867b5c4d8a11ebf6d21a886c065f0e49
devSunni <yangsunmo@gmail.com>
2023-04-22 23:47:24 +0900

    success_boolean 플로이드 워셜

--- CertiBase/CertiBase/CertiBase/CertiBase.cpp
+++ CertiBase/CertiBase/CertiBase/CertiBase.cpp
@@ -2,73 +2,51 @@
 
 using namespace std;
 
-const int max_int = 214783647;
-int N;
-int M;
-int dist[501][501];
+bool d[501][501];
+int N, M, ans;
 
-
-void init()
-{
-    for (int i = 1; i < 501; i++) {
-        for (int j = 1; j < 501; j++)
-        {
-            dist[i][j] = max_int;
-        }
-
-    }
-}
-int main()
-{
-
-    init();
-    scanf("%d %d", &N, &M);
-
-    int a, b;
-
-    for (int m = 0; m < M; m++)
-    {
-        scanf("%d %d", &a, &b);
-        dist[a][b] = 1;
-    }
-
+void solve() {
     for (int i = 1; i <= N; i++) {
+        int cnt = 0;
         for (int j = 1; j <= N; j++) {
-            for (int k = 1; k <= N; k++)
-            {
-                if ((i != k) && (i != j) && (j != k))
-                {
-                    if (dist[i][k] > dist[i][j] + dist[j][k])
-                        dist[i][k] = dist[i][j] + dist[j][k];
-                }                        
-            }
+            if (d[i][j] || d[j][i]) cnt++;
         }
+
+        if (cnt == N - 1) ans++;
     }
 
-    int cnt = 0;
-    int ans = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 1; j <= N; j++)
-        {
-            if (i != j) {
-                if ((dist[i][j] != max_int) || (dist[j][i] != max_int))
-                {
-                    cnt++;
+    cout << ans << '\n';
+}
+
+void func() {
+    for (int k = 1; k <= N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (d[i][k] && d[k][j]) {
+                    d[i][j] = true;
                 }
             }
-            if ((N - 1) > (N - j + cnt))
-                break;
-
         }
-
-        if (cnt == N - 1)
-            ans++;
-
-        cnt = 0;
     }
+}
 
-    printf("%d", ans);
+void input() {
+    int u, v;
+    cin >> N >> M;
+    while (M--) {
+        cin >> u >> v;
+        d[u][v] = true;
+    }
+}
+
+int main() {
+    cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    input();
+    func();
+    solve();
+
 
     return 0;
 }
